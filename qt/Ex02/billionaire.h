d1d60383b8b786c732813a0e91638becd4642628
Pincinato <thiagoh.pincinato@gmail.com>
2018-10-02 19:07:36 +0200

    ex02 finished

--- qt/Ex02/billionaire.h
+++ qt/Ex02/billionaire.h
@@ -13,49 +13,26 @@ class Billionaire
 {
 
 public:
-    Billionaire();
-    //vector <string> info;
+    Billionaire(){
+        name.clear();
+        fortune.clear();
+        country.clear();
+    }
     string name;
     string fortune;
     string country;
 
    friend istream& operator >>(istream& in, Billionaire &Destination) {
-        char info[512];
-        in.getline(info,512,'\t');
-        Destination.name=info;
-        in.getline(info,512,'\t');
-        Destination.fortune=info;
-        in.getline(info,512,'\n');
-        Destination.country=info;
-        //copy(istream_iterator<string>(test),istream_iterator<string>(),back_inserter(Destination.info));
-       /*in>>Destination.first_name;
-       */
+
+        getline(in,Destination.name,'\t');
+        getline(in,Destination.fortune,'\t');
+        getline(in,Destination.country,'\n');
        return in;
-    }
-    friend ostream& operator <<(ostream& out, vector<Billionaire>::iterator Source) {
-
-        string space(" ");
-        //out<<Source.info<<"\n";
-        cout<<"ostream& out, vector<Billionaire>::iterator Source "<<endl;
-        out<<Source->name <<space<<Source->fortune<<space<<Source->country<<endl;
-        return out;
-    }
-
-    friend ostream& operator <<( vector<Billionaire>::iterator Source,ostream& out) {
-
-        string space(" ");
-        //out<<Source.info<<"\n";        
-        cout<<"vector<Billionaire>::iterator Source,ostream& out "<<endl;
-        out<<Source->name <<space<<Source->fortune<<space<<Source->country<<endl;
-        return out;
     }
 
     friend ostream& operator <<(ostream& out, const Billionaire &Source) { //const is a key word as friend it is.
 
-        string space(" ");
-        //out<<Source.info<<"\n";
-        cout<<"ostream& out, Billionaire Source "<<endl;
-        out<<Source.name <<space<<Source.fortune<<space<<Source.country<<endl;
+        out<<Source.name <<" "<<Source.fortune<<" "<<Source.country;
         return out;
     }
 
--- qt/Ex02/main.cpp
+++ qt/Ex02/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <unordered_map>
 #include <vector>
+#include <unordered_map>
 #include <map>
+#include <algorithm>
 #include "billionaire.h"
 using namespace std;
 
@@ -12,14 +13,13 @@ void ex3(void);
 
 int main()
 {
-    //ex2_a();
-    //ex2_b();
+    ex2_a();
+    ex2_b();
     ex3();
     return 0;
 }
 
 void ex2_a(){
-
  vector <pair < string , string >> vec
         { make_pair ("Homer", " 1111 "),
          make_pair ("Marge", " 2222 "),
@@ -41,9 +41,9 @@ void ex2_a(){
 
 
 void ex2_b(){
-
     vector <pair < string , string >> vec
            { make_pair ("Homer", " 1111 "),
+            make_pair ("Magy", " 54545 "),
             make_pair ("Homer", " 11122 "),
             make_pair ("Homer", " 11133 "),
             make_pair ("Marge", " 2222 "),
@@ -66,27 +66,34 @@ void ex2_b(){
 }
 
 void ex3(void){
-
     ifstream in("Forbes2018.txt");
-    cout<<"Exercise 3"<<endl;
+    cout<<endl<<"Exercise 3 "<<endl;
     if (!in) cout << " WARNING : File not found !" << endl ;
     else{
         vector <Billionaire> billionaires ;
         copy(istream_iterator <Billionaire>(in), istream_iterator <Billionaire>(),back_inserter(billionaires));
-        copy (billionaires.begin() ,billionaires.end(),ostream_iterator<Billionaire>(cout));
-        //cout<<billionaires.size();
-        cout<<endl<<"Second part "<<endl;
+        copy_n(billionaires.begin() ,10,ostream_iterator<Billionaire>(cout,"\n"));
         map < string , pair < const Billionaire , size_t >> mymap;
         for(auto &it : billionaires){
-           auto b= mymap.try_emplace(it.country,make_pair(it,1));
-            if(b.second ==false){
-                //b.first.;
-                int abc;
-                abc=0;
+           auto it_pair= mymap.emplace(it.country,make_pair(it,1));
+            if(it_pair.second ==false){
+                it_pair.first->second.second++;
             }
-
         }
+        cout<<"....."<<endl;
+        cout<<endl<<"My map - first 5 positions "<<endl;
+        copy_n(mymap.begin(),6,ostream_iterator<pair<string,pair<const Billionaire,size_t>>>(cout,"\n"));
+        vector <pair<string ,pair <Billionaire, size_t >>> vec(mymap.begin(),mymap.end());
+        cout<<endl<<"The richest person - separated by Country"<<endl;
+        sort(vec.begin(),vec.end(),[](pair<string ,pair < Billionaire, size_t >> &a,pair<string ,pair < Billionaire, size_t >> &b){return (stoi(a.second.first.fortune,nullptr,10)>stoi(b.second.first.fortune,nullptr,10));});
+        copy_n(vec.begin(),6,ostream_iterator<pair<string,pair<const Billionaire,size_t>>>(cout,"\n"));
+        cout<<endl<<"Country with the most billionaires"<<endl;
+        sort(vec.begin(),vec.end(),[](pair<string ,pair < Billionaire, size_t >> &a,pair<string ,pair < Billionaire, size_t >> &b){return (a.second.second>b.second.second);});
+        copy_n(vec.begin(),6,ostream_iterator<pair<string,pair<const Billionaire,size_t>>>(cout,"\n"));
     }
 }
 
-
+ostream& operator <<(ostream& out, const pair<string, pair<const Billionaire, size_t>> &Source) {
+    out<<Source.second.first.country<<" : "<<Source.second.second<<" billionaires. Richest is "<<Source.second.first.name<<" with "<<Source.second.first.fortune<<" B$";
+    return out;
+}
